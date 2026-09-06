b24ac8cf7ca9b8f53c845d84379f400924ec3cb9
Malik05255 <mmbbrr99@gmail.com>
2026-09-07 01:40:51 +0300

    Test balanced hourly cloud audio planning

--- translator-app/src/test/kotlin/com/manzl/movietranslator/CloudAudioPlanTest.kt
+++ translator-app/src/test/kotlin/com/manzl/movietranslator/CloudAudioPlanTest.kt
@@ -14,21 +14,33 @@ class CloudAudioPlanTest {
     }
 
     @Test
-    fun twoHourMovie_usesOnePart() {
-        val duration = 2L * 60L * 60_000L
+    fun oneHourFortyFive_balancesTwoParts() {
+        val duration = 105L * 60_000L
         val parts = CloudAudioExtractor.planParts(duration)
 
-        assertEquals(1, parts.size)
-        assertEquals(0L to duration, parts[0])
+        assertEquals(2, parts.size)
+        assertEquals(duration, parts.sumOf { it.second })
+        assertEquals(parts[0].second, parts[1].second)
     }
 
     @Test
-    fun threeHourMovie_usesExactlyTwoParts() {
+    fun twoHourMovie_usesTwoOneHourParts() {
+        val hour = 60L * 60_000L
+        val parts = CloudAudioExtractor.planParts(2L * hour)
+
+        assertEquals(2, parts.size)
+        assertEquals(0L to hour, parts[0])
+        assertEquals(hour to hour, parts[1])
+    }
+
+    @Test
+    fun threeHourMovie_usesThreeOneHourParts() {
         val hour = 60L * 60_000L
         val parts = CloudAudioExtractor.planParts(3L * hour)
 
-        assertEquals(2, parts.size)
-        assertEquals(0L to 2L * hour, parts[0])
-        assertEquals(2L * hour to hour, parts[1])
+        assertEquals(3, parts.size)
+        assertEquals(0L to hour, parts[0])
+        assertEquals(hour to hour, parts[1])
+        assertEquals(2L * hour to hour, parts[2])
     }
 }
