c3c073380059099673855eaf77208b51eb246740
Malik05255 <mmbbrr99@gmail.com>
2026-09-06 19:46:58 +0300

    fix: use AndroidX OptIn for Media3 APIs

--- translator-app/src/main/kotlin/com/manzl/movietranslator/MainActivity.kt
+++ translator-app/src/main/kotlin/com/manzl/movietranslator/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.OptIn
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
